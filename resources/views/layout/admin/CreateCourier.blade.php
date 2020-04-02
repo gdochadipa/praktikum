fbd17a1687f18b792b83c21beefb3f57a41896f2
gdochadipa <gdochadipa@gmail.com>
2020-04-02 09:56:37 +0800

    ada

--- resources/views/layout/admin/CreateCourier.blade.php
+++ resources/views/layout/admin/CreateCourier.blade.php
@@ -6,15 +6,50 @@
     active
 @endsection
 @section('content'))
-<form action="/tambahcourier"method="POST">
-	{{ csrf_field() }}
-	<p>
-		<label for="Courier">Courier</label>
-		<input type="text" name="courier" >
-	</p>
-	<p>
-		<input type="submit" value="Simpan">
-		<input type="button" value="Kembali" onclick="location.href="/courier"">
-	</p>
-</form>
+<style>
+.row{
+  margin: 10px;
+}
+
+.bmd-label-floating{
+  margin-bottom: 5px;
+}
+</style>
+
+ <div class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card">
+                <div class="card-header card-header-rose">
+                    <i class="material-icons">people_alt</i>
+                  <h4 class="card-title "> </h4>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-12">
+						
+						</div>
+                    </div>
+                    <form action="/tambahcourier"method="POST" enctype="multipart/form-data" >
+                        @csrf
+                      <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group bmd-form-group">
+                                    <label class="bmd-label-floating">Courier</label>
+                                    <input type="text" name="nim" class="form-control" >
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <input type="submit" value="Tambah" class="btn btn-success pull-right">
+                    </form>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+      
 @endsection
